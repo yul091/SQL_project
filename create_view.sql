c61ca82f9dfebecef15aad83abf1362b115e5e6d
yul091 <yul091@ucsd.edu>
2020-11-30 20:17:44 -0600

    'final_debug'

--- create_view.sql
+++ create_view.sql
@@ -8,48 +8,67 @@ AND Pay.MemNo = M.MemNo
 GROUP BY M.MemNo
 ORDER BY SUM(Pay.Amount) DESC LIMIT 3;
 
-CREATE VIEW Top3ID AS
-SELECT C.ID, T.StartDate
+CREATE VIEW AnnualTopMembers AS 
+SELECT P.Fname, P.Lname, T.StartDate
+FROM person AS P, 
+(SELECT C.ID, T.StartDate
 FROM customer AS C, top3mem AS T
 WHERE C.MemNo = T.MemNo
 UNION
 SELECT E.ID, T.StartDate 
 FROM employee AS E, top3mem AS T
-WHERE E.MemNo = T.MemNo;
-
-CREATE VIEW AnnualTopMembers AS 
-SELECT P.Fname, P.Lname, T.StartDate
-FROM person AS P, top3id AS T
+WHERE E.MemNo = T.MemNo) AS T
 WHERE P.ID = T.ID;
 
 -- Q2
-CREATE VIEW PopularProduct
-AS SELECT P.*
+CREATE VIEW PopularProduct AS 
+SELECT P.*
 FROM product AS P, recordop_e2 AS R, storeorder AS O
-WHERE P.ID=R.ProductID AND R.OrderID=O.ID AND 0 <= DATEDIFF(NOW(), O.Time) <= 30
+WHERE P.ID=R.ProductID AND R.OrderID=O.ID AND (DATEDIFF(NOW(), O.Time) BETWEEN 0 AND 30)
 GROUP BY R.ProductID
 ORDER BY COUNT(*) DESC LIMIT 1;
 
 -- Q3 
-CREATE VIEW PotentialMemberCustomer
-AS SELECT C.*
+CREATE VIEW PotentialMemberCustomer AS 
+SELECT C.*
 FROM customer AS C, storeorder AS O
-WHERE C.MemNo = O.MemNo AND (C.MemNo IS NULL) AND 0 <= DATEDIFF(NOW(), O.Time) <= 30
+WHERE C.MemNo = O.MemNo AND (C.MemNo IS NULL) AND (DATEDIFF(NOW(), O.Time) BETWEEN 0 AND 30)
 GROUP BY O.MemNo
 HAVING COUNT(O.ID) > 10;
 
 -- Q4
-CREATE VIEW GoldStore
-AS SELECT S.* 
+CREATE VIEW GoldStore AS 
+SELECT S.* 
 FROM storeorder AS O, stores_e1 AS S
-WHERE S.Name = O.StoreName AND DATEDIFF(NOW(), O.Time) <= 365
+WHERE S.Name = O.StoreName AND (DATEDIFF(NOW(), O.Time) BETWEEN 0 AND 365)
 GROUP BY O.StoreName
 ORDER BY COUNT(DISTINCT O.MemNo) DESC LIMIT 1;
 
 -- Q5 
-CREATE VIEW TopQuarterCashier
-AS SELECT C.*
+-- version 1
+CREATE VIEW TopQuarterCashier AS 
+SELECT C.*
 FROM cashier AS C, payment_e2 AS P, storeorder AS O
-WHERE C.ID = P.CashierID AND 0 <= DATEDIFF(NOW(), O.time) < 90 AND P.OrderID = O.ID
+WHERE C.ID = P.CashierID AND (DATEDIFF(NOW(), O.Time) BETWEEN 0 AND 90) AND P.OrderID = O.ID
 GROUP BY P.CashierID
 ORDER BY COUNT(O.ID) DESC LIMIT 1;
+-- version 2
+CREATE VIEW TopQuarterCashier2 AS 
+((SELECT C.*
+FROM cashier AS C, payment_e2 AS P, storeorder AS O
+WHERE C.ID = P.CashierID AND (DATEDIFF(NOW(), O.time) BETWEEN 0 AND 30) AND P.OrderID = O.ID
+GROUP BY P.CashierID
+ORDER BY COUNT(O.ID) DESC LIMIT 1)
+UNION
+(SELECT C.*
+FROM cashier AS C, payment_e2 AS P, storeorder AS O
+WHERE C.ID = P.CashierID AND (DATEDIFF(NOW(), O.time) BETWEEN 30 AND 60) AND P.OrderID = O.ID
+GROUP BY P.CashierID
+ORDER BY COUNT(O.ID) DESC LIMIT 1)
+UNION
+(SELECT C.*
+FROM cashier AS C, payment_e2 AS P, storeorder AS O
+WHERE C.ID = P.CashierID AND (DATEDIFF(NOW(), O.time) BETWEEN 60 AND 90) AND P.OrderID = O.ID
+GROUP BY P.CashierID
+ORDER BY COUNT(O.ID) DESC LIMIT 1)
+);
--- queries.sql
+++ queries.sql
@@ -44,31 +44,69 @@ FROM employee AS E, card AS C
 WHERE E.MemNo = C.MemNo AND (DATEDIFF(C.StartDate, E.StartDate) BETWEEN 0 AND 30);
 
 -- Q6
--- SELECT P.Fname, P.Mname, P.Lname
--- FROM Person AS P,
--- (SELECT customer AS C
--- FROM 
--- (SELECT G.MemNo
--- FROM guest AS G
--- GROUP BY G.MemNo
--- ORDER BY COUNT(G.ID) DESC LIMIT 1) AS MaxMem
--- WHERE 
--- ;
+SELECT P.Fname, P.Mname, P.Lname, G.MemNo, COUNT(DISTINCT G.ID)
+FROM Person AS P, Guest AS G, Customer AS C, Employee AS E
+WHERE (G.MemNo = C.MemNo AND P.ID = C.ID) OR (G.MemNo = E.MemNo AND P.ID = E.ID)
+GROUP BY G.MemNo
+ORDER BY COUNT(DISTINCT G.ID) DESC LIMIT 1;
 
+-- Q7
+SELECT S.*, COUNT(DISTINCT R.productID)
+FROM storerecord AS R, stores_e1 AS S
+WHERE S.Name = R.StoreName AND S.FloorNo = R.StoreFloor AND R.Quantity > 0
+GROUP BY R.StoreName, R.StoreFloor
+ORDER BY COUNT(DISTINCT R.productID) DESC LIMIT 1;
 
+-- Q8
+-- version 1
+SELECT P.*, F.MgrID
+FROM person AS P, floorstaff AS F
+WHERE P.ID = F.ID AND F.ID IN 
+(SELECT S.StaffID 
+FROM stores_e2 AS S
+WHERE (DATEDIFF(CURDATE(), S.Day) BETWEEN 0 AND 7) 
+GROUP BY S.StaffID
+HAVING COUNT(DISTINCT S.FloorNo) = (SELECT COUNT(DISTINCT SF.FloorNo) FROM stores_e1 AS SF)
+);
+-- version 2
+SELECT P.*, F.MgrID
+FROM person AS P, floorstaff AS F
+WHERE P.ID = F.ID AND NOT EXISTS (
+SELECT DISTINCT S1.FloorNo
+FROM stores_e1 AS S1
+WHERE S1.FloorNo NOT IN
+(SELECT DISTINCT FloorNo
+FROM stores_e2 AS S2
+WHERE (DATEDIFF(CURDATE(), S2.Day) BETWEEN 0 AND 7) AND F.ID = S2.StaffID
+));
 
+-- Q9
+SELECT P.*, O.StoreName, O.StoreFloor, ROP.Price
+FROM storeorder AS O, recordop_e2 AS ROP, product AS P
+WHERE ROP.OrderID = O.ID and ROP.ProductID = P.ID
+GROUP BY P.ID, O.StoreName, O.StoreFloor;
 
+-- Q10
+SELECT S.FloorNo, COUNT(S.Name)
+FROM stores_e1 AS S
+GROUP BY FloorNo
+ORDER BY COUNT(S.Name) DESC LIMIT 1;
 
+-- Q11
+SELECT ScheduleTable.* 
+FROM GoldStore, ScheduleTable
+WHERE GoldStore.ScheduleID = ScheduleTable.ID;
 
+-- Q12
+SELECT S.*, COUNT(O.ID)
+FROM stores_e1 AS S, storeorder AS O
+WHERE S.Name = O.StoreName AND S.FloorNo = O.StoreFloor AND (DATEDIFF(CURDATE(), O.Time) BETWEEN 0 AND 7)
+GROUP BY O.StoreName, O.StoreFloor
+ORDER BY COUNT(O.ID) DESC LIMIT 1;
 
-
-
-
-
-
-
-
-
-
-
-
+-- Q13
+SELECT P.*, E.MemNo, E.StartDate, COUNT(F.ID)
+FROM person AS P, employee AS E, manager AS M, floorstaff AS F 
+WHERE P.ID = E.ID AND E.ID = M.ID AND F.MgrID = M.ID
+GROUP BY F.MgrID
+ORDER BY COUNT(F.ID) DESC LIMIT 1;
